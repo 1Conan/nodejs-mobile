e8b0e8d3580e8bce679d9eb03b3c50cfe7a8b68d
1Conan <me@1conan.com>
2022-10-04 03:45:40 +0800

    test build

---
+++ tools/ios_vars.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+export LIBRARY_PATH='out/Release'
+
+export LIBRARY_FILES=(
+    libbrotli.a
+    libcares.a
+    libhistogram.a
+    libllhttp.a
+    libnghttp2.a
+    libnghttp3.a
+    libngtcp2.a
+    libnode.a
+    libopenssl.a
+    libtorque_base.a
+    libuv.a
+    libuvwasi.a
+    libv8_base_without_compiler.a
+    libv8_compiler.a
+    libv8_init.a
+    libv8_initializers.a
+    libv8_libbase.a
+    libv8_libplatform.a
+    libv8_snapshot.a
+    libv8_zlib.a
+    libzlib.a
+    libicudata.a
+    libicui18n.a
+    libicustubdata.a
+    libicuucx.a
+)
